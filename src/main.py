ca0048d953d59e2edb5c8842ecedb34f22e82413
kata <kataocean@gmail.com>
2019-12-15 12:42:36 +0900

    すぐにログを出力するように

--- src/main.py
+++ src/main.py
@@ -71,7 +71,7 @@ while True:
         if not list_root:
             break
 
-        for project in tqdm(list_root.select('div.well'), leave=False):
+        for project in list_root.select('div.well'):
             try:
                 title_content = project.select_one('h2.title a')
                 url = title_content.get('href')
